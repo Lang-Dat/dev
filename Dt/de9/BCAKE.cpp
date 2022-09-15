e8922cf3a7be9bbd14e7746cccc9953d61d7d24e
Lang Dat <langtrongtue@gmail.com>
2022-09-15 19:54:37 +0700

    Bai cuoi cua de 9, bai ni dung them mot mang dp va mot so ky thuat nhu printf va M_PI

--- Dt/de9/BCAKE.cpp
+++ Dt/de9/BCAKE.cpp
@@ -1,6 +1,8 @@
 #pragma GCC optimize("O2")
 #include <math.h>
 #include <iostream>
+#include <stdio.h>
+#include <set>
 
 int main()
 {
@@ -9,15 +11,26 @@ int main()
     freopen("./BCAKE.INP", "r", stdin);
     freopen("./BCAKE.OUT", "w", stdout);
 
-    int n, r, h;
+    int n;
+    double r, h;
     std::cin >> n;
-    long double theTich[n];
+    double theTich[n];
     for (int i = 0; i < n; i++) {
         std::cin >> r >> h;
-        theTich[i] = r*r*h*PI;
+        theTich[i] = r*r*h*M_PI;
     }
-    for (auto i : theTich)
-        std::cout << i << " ";
 
+    double dp[n]; // The tich lon nhat neu chon cai do
+    // dp[n - 1] = theTich[n - 1];
+    for (int i = n - 1; i >= 0; i--) {
+        dp[i] = theTich[i];
+        double currMax = 0;
+        for (int j = i + 1; j < n; j++) {
+            if (theTich[j] > theTich[i] && dp[j] > currMax)
+                currMax = dp[j];
+        }
+        dp[i] += currMax;
+    }
+    printf("%.3f", dp[0]);
     return 0;
 }
