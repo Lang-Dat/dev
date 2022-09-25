0f646b53de03522a862dcc5bd48415665a05f79e
Lang Dat <langtrongtue@gmail.com>
2022-09-25 19:40:03 +0700

    Lam de tinh, de ni kho vai ra

---
+++ Dt/de14/Daychiahet.cpp
@@ -0,0 +1,23 @@
+#pragma GCC optimize("O2")
+#include <iostream>
+#include <vector>
+
+std::vector<int> nums;
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    freopen("./Daychiahet.INP", "r", stdin);
+    freopen("./Daychiahet.OUT", "w", stdout);
+
+    int n;
+    std::cin >> n;
+    nums.reserve(n + 1);
+    nums.push_back(1);
+    for (int i = 2; nums.size() < n;i++)
+        for (int count = 0, j = nums.back() / i * i + i; nums.size() < n && count < i; count++, j += i)
+            nums.push_back(j);
+    std::cout << nums[n-1];
+
+    return 0;
+}
