aab3ee92cbc825d24cc6f160fbcadcfbffbdad0e
Lang Dat <langtrongtue@gmail.com>
2022-09-26 20:32:54 +0700

    sua loi lien quan den gioi han cua int 2^32

--- Dt/de15/BAI5.cpp
+++ Dt/de15/BAI5.cpp
@@ -4,16 +4,19 @@
 #include <iostream>
 #include <algorithm>
 
-bool isPrime(int n) {
+// phai dung ull vi voi cac so co 10 chu so thi khi dao nguoc no se bi qua gioi han cua int
+#define ull unsigned long long
+
+bool isPrime(ull n) {
     if (n < 4) return n > 1;
     if (n % 2 == 0 || n % 3 == 0) return false;
-    for (int i = 5; i <= std::sqrt(n); i += 6) 
+    for (ull i = 5; i <= std::sqrt(n); i += 6) 
         if (n % i == 0 || n % (i + 2) == 0)
             return false;
     return true;
 }
-int reverse(int n) {
-    int rev = 0;
+ull reverse(ull n) {
+    ull rev = 0;
     while (n > 0)
     {
         rev = rev * 10 + n % 10;
@@ -28,16 +31,15 @@ int main()
     freopen("./BAI5.INP", "r", stdin);
     freopen("./BAI5.OUT", "w", stdout);
 
-    int p, q;
+    ull p, q;
     std::cin >> p >> q;
-    std::vector<int> ans;
+    std::vector<ull> ans;
     ans.reserve(q - p);
-    for (int i = p; i <= q; i++)
+    for (ull i = p; i <= q; i++)
         if (isPrime(reverse(i)))
             ans.push_back(i);
     std::sort(ans.begin(), ans.end());
-
-    for (int i : ans)
+    for (ull i : ans)
         std::cout << i << "\n";
     
     return 0;
