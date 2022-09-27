e4f3209a7070c0b886fe0de8bff31785344db788
may truong <langtrongtue@gmail.com>
2022-09-27 09:26:07 +0700

    Lam them bai 3, sử dụng mảng cộng dồn TC: O(n)

---
+++ Dt/de16/BAI3.cpp
@@ -0,0 +1,29 @@
+#pragma GCC optimize("O2")
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+const int LIM = 1e6 + 1;
+std::vector<int> nums(LIM, 0);
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    freopen("./BAI3.INP", "r", stdin);
+    freopen("./BAI3.OUT", "w", stdout);
+
+    int n, k, c, x, max = 0;
+    std::cin >> n >> k;
+    k = k * 2 + 1;
+    while (std::cin >> c >> x)
+        nums[x] += c;
+            for (int i = 1; i < LIM; i++)
+                nums[i] += nums[i - 1];
+
+    for (int i = 1; i < LIM; i++)
+        if (i + k < LIM && nums[i + k] - nums[i-1] > max)
+            max = nums[i + k] - nums[i-1];
+    max = std::max(max, nums[LIM - 1] - nums[LIM - k - 1]);
+    std::cout << max;
+    return 0;
+}
