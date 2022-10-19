97c013e6aef0f6b972a10f92485c3607f87cdbe4
May truong <langtrongtue@gmail.com>
2022-10-19 09:26:38 +0700

    Làm được 5 điểm đề tỉnh rồi, full điểm rồi

--- Dt/65/ChiaKeo.cpp
+++ Dt/65/ChiaKeo.cpp
@@ -6,27 +6,32 @@
 #include <algorithm>
 using namespace std;
 
-const int LIM = 2 * 1e5 + 5;
-int nums[LIM];
-int n;
+#define ll long long
 
-int effecient() {
-    int lowest = INT32_MAX;
-    for (int i = 1; i < n; i++) {
-        int bigger = lower_bound(nums + i + 1, nums + n, (nums[n] - nums[i]) / 2 + nums[i]) - nums;
-        int max_val = max({nums[i], nums[bigger] - nums[i], nums[n] - nums[bigger]});
-        int min_val = min({nums[i], nums[bigger] - nums[i], nums[n] - nums[bigger]});
-        lowest = min(lowest, max_val - min_val);
+const ll LIM = 2 * 1e5 + 5;
+ll nums[LIM];
+ll n;
+
+ll min_diff(int bigger, int i) {
+    ll max_val = max({nums[i], nums[bigger] - nums[i], nums[n] - nums[bigger]});
+    ll min_val = min({nums[i], nums[bigger] - nums[i], nums[n] - nums[bigger]});
+    return max_val - min_val;
+}
+ll effecient() {
+    ll lowest = INT32_MAX;
+    for (ll i = 1; i < n; i++) {
+        ll bigger = lower_bound(nums + i + 1, nums + n, (nums[n] - nums[i]) / 2 + nums[i]) - nums;
+        lowest = min({lowest, min_diff(bigger, i), min_diff(bigger - 1, i)});
     }
     return lowest;
 }
-int bruceForce() {
+ll bruceForce() {
     // just use two loop
-    int lowest = INT32_MAX;
-    for (int i = 1; i < n - 1; i++) {
-        for (int j = i + 1; j < n; j++) {
-            int max_val = max({nums[i], nums[j] - nums[i], nums[n] - nums[j]});
-            int min_val = min({nums[i], nums[j] - nums[i], nums[n] - nums[j]});
+    ll lowest = INT64_MAX;
+    for (ll i = 1; i < n - 1; i++) {
+        for (ll j = i + 1; j < n; j++) {
+            ll max_val = max({nums[i], nums[j] - nums[i], nums[n] - nums[j]});
+            ll min_val = min({nums[i], nums[j] - nums[i], nums[n] - nums[j]});
             lowest = min(lowest, max_val - min_val);
         }
     }
@@ -41,12 +46,11 @@ int main()
     freopen("./ChiaKeo.OUT", "w", stdout);
 
     std::cin >> n;
-    for (int i = 1; i <= n; i++) {
+    for (ll i = 1; i <= n; i++) {
         std::cin >> nums[i];
         nums[i] += nums[i-1];
     }
     
-    // std::cout << bruceForce() << " ";
     std::cout << effecient();
     return 0;
 }
