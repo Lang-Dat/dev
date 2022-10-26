fc1687240ad20f509f04745d34b85a08b9ea564e
May truong <langtrongtue@gmail.com>
2022-10-26 09:07:05 +0700

    ...

--- Dt/74/GCDMAX.cpp
+++ Dt/74/GCDMAX.cpp
@@ -5,29 +5,17 @@
 #include <iostream>
 using namespace std;
 
-const int LIM = 1e6 + 6;
-int dp[LIM];
-
 int main()
 {
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(nullptr);
-    // freopen("./GCDMAX.INP", "r", stdin);
-    // freopen("./GCDMAX.OUT", "w", stdout);
-
-    dp[2] = 1;
-    dp[3] = 1;
-    for (int i = 4; i < LIM; i++) {
-        dp[i] = dp[i-1];
-        for (int j = dp[i - 1]; j <= i / 2; j++) {
-            if (i / j > 1) dp[i] = j;           
-        }
-    }
+    freopen("./GCDMAX.INP", "r", stdin);
+    freopen("./GCDMAX.OUT", "w", stdout);
     int t, n;
     std::cin >> t;
     while (std::cin >> n)
     {
-        std::cout << dp[n] << "\n";
+        std::cout << n/2 << "\n";
     }
     
     return 0;
