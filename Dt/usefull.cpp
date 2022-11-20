b1b25d12a39167a38b716b547772e3738ccee6f8
Lang Dat <langtrongtue@gmail.com>
2022-11-20 12:44:52 +0700

    thêm mảng ghi các số hoàn hảo

--- Dt/usefull.cpp
+++ Dt/usefull.cpp
@@ -6,6 +6,7 @@
 
 #define ull unsigned long long
 
+int perfect_num[] = {6, 28, 496, 8128, 33550336};
 // help funcion
 void decresingN(ull &n, ull i, std::unordered_map<ull, ull> &freq_prime) {
     ull tmp = 0;
@@ -28,15 +29,6 @@ std::unordered_map<ull, ull> phanTichN(ull n, std::unordered_map<ull, ull> freq_
     if (n > 1) freq_prime[n] += 1;
     return freq_prime;
 }
-ull sumOfFactor(ull n) {
-    std::unordered_map<ull, ull> freq_prime = phanTichN(n);
-    ull sum = 1;
-    for (const auto &p : freq_prime) {
-        // fix std::pow'bug by + 0.1
-        sum *= ((ull)(std::pow(p.first, p.second + 1) + 0.1) - 1) / (ull)(p.first - 1); 
-    }
-    return sum;
-}
 ull numOfDivisor(ull n) {
     std::unordered_map<ull, ull> freq_prime = phanTichN(n);
         ull sum = 1;
@@ -45,14 +37,23 @@ ull numOfDivisor(ull n) {
     }
     return sum;
 }
+ull sumOfFactor(ull n) {
+    std::unordered_map<ull, ull> freq_prime = phanTichN(n);
+    double sum = 1;
+    for (const auto &p : freq_prime) {
+        // fix std::pow'bug by + 0.1
+        sum *= ((ull)(std::pow(p.first, p.second + 1) + 0.1) - 1) / (double)(p.first - 1); 
+    }
+    return sum;
+}
 ull tongUoc(ull n, ull sum = 0) {
     // bruce force
-    for (ull i = 1; i <= n; i++)
+    for (ull i = 1; i * i<= n; i++)
         // if (n % i == 0 && i * i != n) sum += i + n / i;
         // else if (n % i == 0 && i * i == n) sum += i;
         if (n % i == 0) {
             sum += i;
-            // if (i * i != n) sum += n / i;
+            if (i * i != n) sum += n / i;
         }
     return sum;
 }
@@ -63,7 +64,7 @@ ull tongChuSo(ull n, ull sum = 0) {
     }
     return sum;
 }
-bool isPrime(ull n) {
+bool isPrime_(ull n) {
     if (n <= 3) return n > 1;
     if (n % 2 == 0 || n % 3 == 0) return false;
     for (ull i = 5; i <= std::sqrt(n); i += 6)
@@ -112,3 +113,31 @@ public:
         return index_of_occu;
     }
 };
+
+const int LIM = 1e6 + 6;
+std::vector<bool> isPrime(LIM, true);
+
+void sieve(int n) {
+    isPrime[0] = false;
+    isPrime[1] = false;
+    for (int i = 2; i * i <= n; i++) {
+        if (isPrime[i]) {
+            for (int j = i * i; j <= n; j += i)
+                isPrime[j] = false;
+        }
+    }
+}
+static const int LIM = 2*1e6 + 4;
+int tong_uoc[LIM];
+
+void tinh_tong_uoc() {
+    for (int i = 1; i < LIM; i++) {
+        for (int j = 1; j * i < LIM; j++)
+            tong_uoc[i*j] += i;
+    }
+}
+int main() {
+    for (int i = 1; i < 1e6; i++)
+        if(tongUoc(i) != sumOfFactor(i))
+            db(i)
+}
