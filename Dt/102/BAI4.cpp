697175f6c8ef0fae6260ab3e4427ee0fb856370f
May truong <langtrongtue@gmail.com>
2022-11-21 08:15:30 +0700

    dùng tính chất Log10(a.b) = Log10(a) + Log10(b)

--- Dt/102/BAI4.cpp
+++ Dt/102/BAI4.cpp
@@ -1,9 +1,10 @@
+// Log10(a.b) = Log10(a) + Log10(b)
 /**
  *	author: Lang Dat
  *	create: 20-11-2022 03:21:19
 **/
 #include <iostream>
-#include "BigInt.hpp"
+#include <cmath>
 using namespace std;
 
 int main()
@@ -11,10 +12,19 @@ int main()
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(nullptr);
     #ifndef ONLINE_JUDGE
-    // freopen("./BAI4.INP", "r", stdin);
-    freopen("./BAI4.txt", "w", stdout);
+    freopen("./BAI4.INP", "r", stdin);
+    freopen("./BAI4.OUT", "w", stdout);
     #endif
 
-
+    int n, i;
+    double count = 0;
+    std::cin >> n;
+    for (i = 1; count < n - 1; i++) {
+        count += log10(i);
+    }
+    if ((int)count == n - 1)
+        cout << i - 1;
+    else 
+        cout << -1;
     return 0;
 }
