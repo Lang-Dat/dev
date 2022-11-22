5c53cfda9f1b98fcd0b304ea97b4837c0dd84a28
Lang Dat <langtrongtue@gmail.com>
2022-11-22 21:53:18 +0700

    Bị cái chi đó mà sửa chưa được

--- Dt/100/BAI5.cpp
+++ Dt/100/BAI5.cpp
@@ -6,6 +6,8 @@
 #include <algorithm>
 using namespace std;
 
+#define ull unsigned long long
+
 static const int LIM = 1e6 + 5;
 int prefix[LIM];
 
@@ -15,10 +17,11 @@ int main()
     std::cin.tie(nullptr);
     #ifndef ONLINE_JUDGE
     freopen("./BAI5.INP", "r", stdin);
+    // freopen("./Test/BAI5/Test03/BAI5.INP", "r", stdin);
     freopen("./BAI5.OUT", "w", stdout);
     #endif
 
-    int k, count = 0, n;
+    ull k, ans = 0, n;
     string str;
     std::cin >> k >> str;
     n = str.size();
@@ -26,15 +29,13 @@ int main()
         prefix[i] = prefix[i-1];
         if (str[i-1] == '1') prefix[i]++;
     }
-
     for (int i = 0; i < n + 1; i++) {
-        int ind = lower_bound(prefix + i + 1, prefix + n + 1, prefix[i] + k) - prefix;
+        ull ind = lower_bound(prefix + i + 1, prefix + n + 1, prefix[i] + k) - prefix;
         if (prefix[ind] - prefix[i] == k) {
-            int end = lower_bound(prefix + ind + 1, prefix + n + 1, prefix[ind] + 1) - prefix;
-            count += end - ind + 1;
-            if (end == n + 1) count--;
+            ull end = lower_bound(prefix + ind + 1, prefix + n + 1, prefix[ind] + 1) - prefix;
+            ans += end - ind;
         }
     }
-    std::cout << count << "";
+    std::cout << ans << "";
     return 0;
 }
