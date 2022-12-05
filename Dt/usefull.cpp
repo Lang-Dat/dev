fd1f09a834fa47aaf38d06f54184bc983e11aada
Lang Dat <langtrongtue@gmail.com>
2022-12-05 22:00:06 +0700

    thêm hàm sinh số ước của một số (cho nhiều truy vấn

--- Dt/usefull.cpp
+++ Dt/usefull.cpp
@@ -4,12 +4,14 @@
 #include <unordered_map>
 #include <vector>
 
-#define ull unsigned long long
+#define uint64_t unsigned long long
 
+static const int MAXX = 1e6;
+static const int SUM_OF_PRIME = 37550402023;
 int perfect_num[] = {6, 28, 496, 8128, 33550336};
 // help funcion
-void decresingN(ull &n, ull i, std::unordered_map<ull, ull> &freq_prime) {
-    ull tmp = 0;
+void decresingN(uint64_t &n, uint64_t i, std::unordered_map<uint64_t, uint64_t> &freq_prime) {
+    uint64_t tmp = 0;
     while (n % i == 0) {
         n /= i;
         tmp++;
@@ -19,36 +21,36 @@ void decresingN(ull &n, ull i, std::unordered_map<ull, ull> &freq_prime) {
     }
 }
 // Phân tích n ra thừa số nguyên tố và trả về umpii
-std::unordered_map<ull, ull> phanTichN(ull n, std::unordered_map<ull, ull> freq_prime = {}) {
+std::unordered_map<uint64_t, uint64_t> phanTichN(uint64_t n, std::unordered_map<uint64_t, uint64_t> freq_prime = {}) {
     decresingN(n, 2, freq_prime);
     decresingN(n, 3, freq_prime);
-    for (ull i = 5; i * i <= n; i += 6) {
+    for (uint64_t i = 5; i * i <= n; i += 6) {
         decresingN(n, i, freq_prime);
         decresingN(n, i+2, freq_prime);
     }
     if (n > 1) freq_prime[n] += 1;
     return freq_prime;
 }
-ull numOfDivisor(ull n) {
-    std::unordered_map<ull, ull> freq_prime = phanTichN(n);
-        ull sum = 1;
+uint64_t numOfDivisor(uint64_t n) {
+    std::unordered_map<uint64_t, uint64_t> freq_prime = phanTichN(n);
+        uint64_t sum = 1;
     for (const auto &p : freq_prime) {
         sum *= (p.second + 1); 
     }
     return sum;
 }
-ull sumOfFactor(ull n) {
-    std::unordered_map<ull, ull> freq_prime = phanTichN(n);
+uint64_t sumOfFactor(uint64_t n) {
+    std::unordered_map<uint64_t, uint64_t> freq_prime = phanTichN(n);
     double sum = 1;
     for (const auto &p : freq_prime) {
         // fix std::pow'bug by + 0.1
-        sum *= ((ull)(std::pow(p.first, p.second + 1) + 0.1) - 1) / (double)(p.first - 1); 
+        sum *= ((uint64_t)(std::pow(p.first, p.second + 1) + 0.1) - 1) / (double)(p.first - 1); 
     }
     return sum;
 }
-ull tongUoc(ull n, ull sum = 0) {
+uint64_t tongUoc(uint64_t n, uint64_t sum = 0) {
     // bruce force
-    for (ull i = 1; i * i<= n; i++)
+    for (uint64_t i = 1; i * i<= n; i++)
         // if (n % i == 0 && i * i != n) sum += i + n / i;
         // else if (n % i == 0 && i * i == n) sum += i;
         if (n % i == 0) {
@@ -57,34 +59,34 @@ ull tongUoc(ull n, ull sum = 0) {
         }
     return sum;
 }
-ull tongChuSo(ull n, ull sum = 0) {
+uint64_t tongChuSo(uint64_t n, uint64_t sum = 0) {
     while(n > 0) {
         sum += n % 10;
         n /= 10;
     }
     return sum;
 }
-bool isPrime_(ull n) {
+bool isPrime_(uint64_t n) {
     if (n <= 3) return n > 1;
     if (n % 2 == 0 || n % 3 == 0) return false;
-    for (ull i = 5; i <= std::sqrt(n); i += 6)
+    for (uint64_t i = 5; i <= std::sqrt(n); i += 6)
         if (n % i == 0 || n % (i + 2) == 0) return false;
     return true;
 }
-inline bool isPerfectSQ(ull n) {
-    return (ull)std::sqrt(n) * (ull)std::sqrt(n) == n;
+inline bool isPerfectSQ(uint64_t n) {
+    return (uint64_t)std::sqrt(n) * (uint64_t)std::sqrt(n) == n;
 }
-ull v_to_ull(std::vector<ull> number) {
-    // convert vector contain single digit to ulleger
-    ull num = 0;
-    for (ull i : number) {
+uint64_t v_to_uint64_t(std::vector<uint64_t> number) {
+    // convert vector contain single digit to uint64_teger
+    uint64_t num = 0;
+    for (uint64_t i : number) {
         num = num * 10 + i;
     }
     return num;
 }
 class KMP {
     const std::string &pattern;
-    ull m;
+    uint64_t m;
     std::vector<int> table;
 public:
     KMP(const std::string &pattern)
@@ -130,6 +132,15 @@ void sieve(int n) {
 static const int LIM = 2*1e6 + 4;
 int tong_uoc[LIM];
 
+static const int LIM_VALUE = 1e6;
+int divisors[LIM_VALUE]; // divisors[i]: number of divisor of i
+
+void sieveDivisor() {
+    // Time complexity: O(nLog2(n)) (n is LIM_VALUE)
+    for (int i = 1; i < LIM_VALUE; i++)
+        for (int j = i; j < LIM_VALUE; j += i)
+            divisors[j]++;
+}
 void tinh_tong_uoc() {
     for (int i = 1; i < LIM; i++) {
         for (int j = 1; j * i < LIM; j++)
