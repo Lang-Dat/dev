0365985eec12c00c44a23dce56020300da3298e3
Lang Dat <langtrongtue@gmail.com>
2022-12-07 20:23:20 +0700

    thêm hàm tìm LIS trong O(nLog2n)

--- Dt/usefull.cpp
+++ Dt/usefull.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <unordered_map>
 #include <vector>
+#include <algorithm>
+
 
 #define uint64_t unsigned long long
 
@@ -145,6 +147,20 @@ void tinh_tong_uoc() {
     for (int i = 1; i < LIM; i++) {
         for (int j = 1; j * i < LIM; j++)
             tong_uoc[i*j] += i;
+    }
+}
+
+int lengthOfLIS(int *nums, int n) {
+    static const int INF = INT32_MAX;
+    std::vector<int> dp(n+2, INF); // dp[i]: phần tử nhỏ nhất mà tại đó một dãy con độ dài i kết thúc
+    dp[0] = -INF;
+    for (int i = 0; i < n; i++) {
+        int j = std::upper_bound(dp.begin(), dp.end(), nums[i]) - dp.begin();
+        if (dp[j-1] < nums[i] && nums[i] < dp[j])
+            dp[j] = nums[i];
+    }
+    for (int i = 0; i <= n; i++) {
+        if (dp[i+1] == INF) return i;
     }
 }
 int main() {
