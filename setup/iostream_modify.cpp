9aec2890a56cf0c730b198c32ad42ecf15aeb01a
Lang Dat <langtrongtue@gmail.com>
2023-01-16 08:06:45 +0700

    sửa vài cái

--- setup/iostream_modify.cpp
+++ setup/iostream_modify.cpp
@@ -1,96 +1,19 @@
-
-// Addition modify
-#include <set>
-#include <memory>
-#include <chrono>
-#include <vector>
-#include <map>
-#include <utility>
-
-#define logm(mess) std::cout << mess;
-#define log1(a) std::cout << #a << " = " << a << "\n";
-#define log2(a, b) std::cout << #a << " = " << a << " " \
+#define dbm(mess) std::cout << mess;
+#define db1(a) std::cout << #a << " = " << a << "\n";
+#define db2(a, b) std::cout << #a << " = " << a << " " \
                              << #b << " = " << b << "\n";
-#define log3(a, b, c) std::cout << #a << " = " << a << " " \
+#define db3(a, b, c) std::cout << #a << " = " << a << " " \
                              << #b << " = " << b << " " \
                              << #c << " = " << c << "\n";
-#define log4(a, b, c, d) std::cout << #a << " = " << a << " " \
+#define db4(a, b, c, d) std::cout << #a << " = " << a << " " \
                              << #b << " = " << b << " " \
                              << #c << " = " << c << " " \
                              << #d << " = " << d << "\n";
-#define log5(a, b, c, d, e) std::cout << #a << " = " << a << " " \
+#define db5(a, b, c, d, e) std::cout << #a << " = " << a << " " \
                              << #b << " = " << b << " " \
                              << #c << " = " << c << " " \
                              << #d << " = " << d << " " \
                              << #e << " = " << e << "\n";
 
 #define GET_MACRO(_1,_2,_3,_4,_5,NAME,...) NAME
-#define log(...) GET_MACRO(__VA_ARGS__, log5, log4, log3, log2, log1)(__VA_ARGS__)
-
-struct Timer
-{
-    // dependents header: chrono
-public:
-    bool inSecond;
-    std::chrono::_V2::system_clock::time_point start, end;
-    std::chrono::duration<float> duration;
-
-    Timer()
-    {
-        start = std::chrono::high_resolution_clock::now();
-    }
-
-    ~Timer()
-    {
-        end = std::chrono::high_resolution_clock::now();
-        duration = end - start;
-        float ms = duration.count() * 1000;
-        std::cout << "Running time: " << ms << "ms ~ " 
-                  << duration.count() << "s\n";
-    }
-};
-
-// Print set with cout
-template<typename T>
-std::ostream &operator<<(std::ostream &os, const std::set<T> &s)
-{
-    os << "{ ";
-    for (auto &i : s) 
-        os << i << " ";
-    os << "}\t";
-    return os;
-}
-
-// Print vector with cout
-template<typename T>
-std::ostream &operator<<(std::ostream &os, const std::vector<T> &v)
-{
-    os << "{ ";
-    for (auto &i : v) 
-        os << i << " ";
-    os << "}\n";
-    return os;
-}
-// Print 2D vector with cout
-template<typename T>
-std::ostream &operator<<(std::ostream &os, const std::vector<std::vector<T>> &v)
-{
-    os << "\n  ";
-    for (auto &temp : v) 
-    {
-        for (auto &i : temp)
-                os << i << " ";
-        os << "\n  ";
-    }
-    return os;
-}
-// Print map with cout
-template<typename T>
-std::ostream &operator<<(std::ostream &os, const std::map<T, T> &mp)
-{
-    os << "{ ";
-    for (auto &i : mp)
-        os << i.first << " : " << i.second << "; ";
-    os << "}\t";
-    return os;
-}
+#define db(...) GET_MACRO(__VA_ARGS__, db5, db4, db3, db2, db1)(__VA_ARGS__)
